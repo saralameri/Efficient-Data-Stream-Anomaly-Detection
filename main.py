2bb9fecdac8164ab7434dc20f45a5808dc9450b1
saralameri <72477930+saralameri@users.noreply.github.com>
2024-10-24 01:00:25 +0400

    visualization

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 import random
+import matplotlib.pyplot as plt
 
+# Simulating data stream 
 def add_extreme_noise(value, anomaly_factor_range=(5, 10)):
     # Create an extreme anomaly by multiplying the value by a large factor (5x to 10x by default)
     anomaly_factor = random.uniform(*anomaly_factor_range)
@@ -40,8 +42,53 @@ def start_data_stream():
         # Sleep for half a second 
         time.sleep(0.5)
 
+# Starting data stream simulation
 data_stream = start_data_stream()
-for value in data_stream:
+
+# Visualization setup
+plt.ion()  # Turn on interactive mode for live updates
+fig, ax = plt.subplots()
+line, = ax.plot([], [], lw=2, color='black') # Create an empty line object that we will update with new data
+ax.set_xlim(0, 100)
+ax.set_ylim(0, 12000)  
+ax.set_xlabel('Transactions')  # Label for the x-axis
+ax.set_ylabel('Value')         # Label for the y-axis
+ax.set_title('Real-Time Data Stream with Anomalies')  # Title of the plot
+
+x_data = []
+y_data = []
+
+# Anomalies detection
+for index, value in enumerate(data_stream):
+    anomaly = False
     print(value)
-    #ToDo do something with value
+    #ToDo do something with value (if anomaly -> anomaly = Talse)
     ...
+    # Update the data for plotting
+    x_data.append(index)
+    y_data.append(value)
+
+    # Update the plot
+    line.set_data(x_data, y_data) # Update the plot with the new data
+    ax.scatter(x_data, y_data, color='red' if anomaly else 'green', s=50 if anomaly else 20, edgecolor='black', zorder=2)  # Add circle markers
+    ax.relim() # Adjust axis limits based on new data
+    ax.autoscale_view() # Automatically rescale the plot to fit the new data
+
+    plt.draw() # Redraw the updated plot
+    plt.pause(0.01)  # Pause to allow for the plot update
+
+    # Limit the x-axis to show the last 100 data points for better visualization
+    if len(x_data) > 100:
+        ax.set_xlim(index - 100, index)  # Shift x-axis to the right
+
+        # only keep the last 100 points to make the x-axis range more manageable
+        x_data = x_data[-100:]
+        y_data = y_data[-100:]
+
+    # Visualization 
+    # plt.plot(index, value)
+    # animation = FuncAnimation(plt.gcf(), interval = 1000, frames = 500, repeat = False)
+
+# ! will never get to this line  
+# plt.ioff()  # Turn off interactive mode
+# plt.show()
